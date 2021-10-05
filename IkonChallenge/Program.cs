95f99a58659ee31ef0d1ea64665de3986cf433bb
Roberto Avalos <ravalosv@gmail.com>
2021-10-05 15:37:46 -0500

    Versión entregable

---
+++ IkonChallenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IkonChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessCalculator proceso = new ProcessCalculator();
+            proceso.Start();
+
+            Console.WriteLine("Enter para terminar...");
+            Console.ReadLine();
+        }
+    }
+}
